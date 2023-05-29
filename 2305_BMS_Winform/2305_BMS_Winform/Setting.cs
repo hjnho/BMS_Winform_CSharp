a0c6f6eb87c19f7c0a94cc71efd3b12ad692ee3c
404-NOT-FOUND <nhobk@naver.com>
2023-05-29 18:24:45 +0900

    Delegent Event handler 작성

--- 2305_BMS_Winform/2305_BMS_Winform/Setting.cs
+++ 2305_BMS_Winform/2305_BMS_Winform/Setting.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace _2305_BMS_Winform
 {
     public partial class Setting : Form
@@ -19,12 +21,20 @@ namespace _2305_BMS_Winform
 
         private void button_SettingApply_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button_SettingCancel_Click(object sender, EventArgs e)
         {
 
         }
+
+        public delegate void DataPassEventHandler_INT(int data[5]);
+
+        public event DataPassEventHandler_INT DataPassEventInt;
+
+        public delegate void DataPassEventHandler_DOUBLE(double data[5]);
+
+        public event DataPassEventHandler_DOUBLE DataPassEventDouble;
     }
 }
